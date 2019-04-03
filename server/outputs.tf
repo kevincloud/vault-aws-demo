d61f4d850d9d7790705ff1098b15359ba05ab070
Kevin Cochran <kcochran@hashicorp.com>
2019-04-03 19:12:26 -0400

    remove dupes

--- server/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vault-ip" {
-    value = "${aws_instance.vault-server.public_ip}"
-}
-
-output "vault-login" {
-    value = "ssh -i ~/keys/${var.key_pair}.pem ubuntu@${aws_instance.vault-server.public_ip}"
-}
