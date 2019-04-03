b41fdf60adaaed9f460321587de3539832587788
Kevin Cochran <kcochran@hashicorp.com>
2019-04-03 19:10:59 -0400

    restructure

---
+++ scripts/vault_install.sh
@@ -0,0 +1,88 @@
+#!/bin/sh
+# Configures the Vault server for a database secrets demo
+
+# cd /tmp
+echo "Preparing to install Vault..."
+sudo apt-get -y update > /dev/null 2>&1
+sudo apt-get -y upgrade > /dev/null 2>&1
+sudo apt-get install -y unzip jq cowsay mysql-client > /dev/null 2>&1
+sudo apt-get install -y python3 python3-pip
+pip3 install awscli
+
+mkdir /etc/vault.d
+mkdir -p /opt/vault
+mkdir -p /root/.aws
+
+sudo bash -c "cat >/root/.aws/config" << 'EOF'
+[default]
+aws_access_key_id=${AWS_ACCESS_KEY}
+aws_secret_access_key=${AWS_SECRET_KEY}
+EOF
+sudo bash -c "cat >/root/.aws/credentials" << 'EOF'
+[default]
+aws_access_key_id=${AWS_ACCESS_KEY}
+aws_secret_access_key=${AWS_SECRET_KEY}
+EOF
+
+echo "Installing Vault..."
+wget https://releases.hashicorp.com/vault/1.1.0/vault_1.1.0_linux_amd64.zip
+sudo unzip vault_1.1.0_linux_amd64.zip -d /usr/local/bin/
+
+# Server configuration
+sudo bash -c "cat >/etc/vault.d/vault.hcl" << 'EOF'
+storage "file" {
+  path = "/opt/vault"
+}
+
+listener "tcp" {
+  address     = "0.0.0.0:8200"
+  tls_disable = 1
+}
+
+ui = true
+EOF
+
+# Set Vault up as a systemd service
+echo "Installing systemd service for Vault..."
+sudo bash -c "cat >/etc/systemd/system/vault.service" << 'EOF'
+[Unit]
+Description=Hashicorp Vault
+After=network.target
+
+[Service]
+Type=simple
+User=root
+WorkingDirectory=/root
+ExecStart=/usr/local/bin/vault server -config=/etc/vault.d/vault.hcl
+Restart=on-failure # or always, on-abort, etc
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl start vault
+sudo systemctl enable vault
+export VAULT_ADDR=http://localhost:8200
+vault operator init -recovery-shares=1 -recovery-threshold=1 > /root/init.txt 2>&1
+export VAULT_TOKEN=`cat /root/init.txt | sed -n -e '/^Initial Root Token/ s/.*\: *//p'`
+
+# echo "Setting up environment variables..."
+echo "export VAULT_ADDR=http://localhost:8200" >> /home/ubuntu/.profile
+echo "export VAULT_TOKEN=$VAULT_TOKEN" >> /home/ubuntu/.profile
+echo "export VAULT_ADDR=http://localhost:8200" >> /root/.profile
+echo "export VAULT_TOKEN=$VAULT_TOKEN" >> /root/.profile
+
+vault operator unseal `cat /root/init.txt | sed -n -e '/^Unseal Key 1/ s/.*\: *//p'`
+vault operator unseal `cat /root/init.txt | sed -n -e '/^Unseal Key 2/ s/.*\: *//p'`
+vault operator unseal `cat /root/init.txt | sed -n -e '/^Unseal Key 3/ s/.*\: *//p'`
+vault login $VAULT_TOKEN
+vault secrets enable -path="secret" -version=2 kv
+
+# Add our AWS secrets
+curl \
+    --header "X-Vault-Token: $VAULT_TOKEN" \
+    --request POST \
+    --data '{"data": { "aws_access_key": "${AWS_ACCESS_KEY}", "aws_secret_key": "${AWS_SECRET_KEY}" } }' \
+    http://127.0.0.1:8200/v1/secret/data/aws
+
+echo "Vault installation complete."
