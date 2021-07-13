99d03eea09f47a82d6c8a3cb5739ef277c6da71b
Kevin Cochran <kcochran@hashicorp.com>
2021-07-13 13:43:33 -0400

    convert auth from ec2 to iam

--- scripts/ec2_install.sh
+++ scripts/ec2_install.sh
@@ -8,21 +8,83 @@ sudo apt-get -y upgrade > /dev/null 2>&1
 sudo apt-get install -y unzip jq python3 python3-pip > /dev/null 2>&1
 pip3 install awscli
 
+sudo bash -c "cat >/root/sign_request.py" <<EOT
+#!/usr/bin/env python
+# -What-------------------------------------------------------------------------
+# This script creates a request to the AWS Security Token Service API
+# with the action "GetCallerIdentity" and then signs the request using the
+# AWS credentials. It was modified from the python 2.x example published by
+# J. Thompson, the author of the Vault IAM auth method, at the vault support
+# mailing list. https://groups.google.com/forum/#!topic/vault-tool/Mfi3O-lW60I
+# -Why--------------------------------------------------------------------------
+# We are using python here instead of bash to take advantage of the boto3 library
+# which facilitates this work by an order of magnitude
+# -What-for---------------------------------------------------------------------
+# This is useful for authenticating to Vault, because a client can use
+# this script to generate this request and this request is sent with the
+# login attempt to the Vault server. Vault then executes this request and gets
+# the response from GetCallerIdentity, which tells who is trying to authenticate
+# ------------------------------------------------------------------------------
+
+import botocore.session
+from botocore.awsrequest import create_request_object
+import json
+import base64
+import sys
+
+def headers_to_go_style(headers):
+    retval = {}
+    for k, v in headers.items():
+        if isinstance(v, bytes):
+            retval[k] = [str(v, 'utf-8')]
+        else:
+            retval[k] = [v]
+    return retval
+
+def generate_vault_request(awsIamServerId):
+    session = botocore.session.get_session()
+    client = session.create_client('sts')
+    endpoint = client._endpoint
+    operation_model = client._service_model.operation_model('GetCallerIdentity')
+    request_dict = client._convert_to_request_dict({}, operation_model)
+
+    request_dict['headers']['X-Vault-AWS-IAM-Server-ID'] = awsIamServerId
+
+    request = endpoint.create_request(request_dict, operation_model)
+
+    return {
+        'iam_http_request_method': request.method,
+        'iam_request_url':         str(base64.b64encode(bytes(request.url, 'utf-8')), 'utf-8'),
+        'iam_request_body':        str(base64.b64encode(bytes(request.body, 'utf-8')), 'utf-8'),
+        'iam_request_headers':     str(base64.b64encode(bytes(json.dumps(headers_to_go_style(dict(request.headers))), 'utf-8')), 'utf-8'), # It's a CaseInsensitiveDict, which is not JSON-serializable
+    }
+
+if __name__ == "__main__":
+    awsIamServerId = sys.argv[1]
+    print(json.dumps(generate_vault_request(awsIamServerId)))
+EOT
+
+
 sudo bash -c "cat >/root/s1_vault_login.sh" <<EOT
 #!/bin/bash
 clear
 cat <<DESCRIPTION
 Our application can login without needing to pass secrets.
 
-# Get instance signature
-pkcs7=\\\$(curl -s \\\\
-  "http://169.254.169.254/latest/dynamic/instance-identity/pkcs7" | tr -d '\n')
+# Get signed URL
+signed_request=\$(python3 /root/sign-request.py ${VAULT_IP})
+iam_request_url=\$(echo $signed_request | jq -r .iam_request_url)
+iam_request_body=\$(echo $signed_request | jq -r .iam_request_body)
+iam_request_headers=\$(echo $signed_request | jq -r .iam_request_headers)
 
 # Create data payload
-data=\\\$(cat <<EOF
+data=$(cat <<EOF
 {
   "role": "app-db-role",
-  "pkcs7": "\\\$pkcs7"
+  "iam_http_request_method": "POST",
+  "iam_request_url": "\$iam_request_url",
+  "iam_request_body": "\$iam_request_body",
+  "iam_request_headers": "\$iam_request_headers"
 }
 EOF
 )
@@ -38,15 +100,20 @@ DESCRIPTION
 
 read -n1 kbd
 
-# Get instance signature
-pkcs7=\$(curl -s \\
-  "http://169.254.169.254/latest/dynamic/instance-identity/pkcs7" | tr -d '\n')
+# Get signed URL
+signed_request=\$(python3 /root/sign-request.py ${VAULT_IP})
+iam_request_url=\$(echo $signed_request | jq -r .iam_request_url)
+iam_request_body=\$(echo $signed_request | jq -r .iam_request_body)
+iam_request_headers=\$(echo $signed_request | jq -r .iam_request_headers)
 
 # Create data payload
-data=\$(cat <<EOF
+data=$(cat <<EOF
 {
   "role": "app-db-role",
-  "pkcs7": "\$pkcs7"
+  "iam_http_request_method": "POST",
+  "iam_request_url": "\$iam_request_url",
+  "iam_request_body": "\$iam_request_body",
+  "iam_request_headers": "\$iam_request_headers"
 }
 EOF
 )
