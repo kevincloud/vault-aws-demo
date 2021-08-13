134d08507c9635d6941a6b45565166806d1fd345
Kevin Cochran <kcochran@hashicorp.com>
2021-08-13 15:25:57 -0400

    add module ref

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 provider "aws" {
     region = var.aws_region
 }
+
+module "jenkinssg" {
+  source  = "app.terraform.io/kevindemos/jenkinssg/aws"
+  version = "1.0.4"
+
+  aws_region = var.aws_region
+}
